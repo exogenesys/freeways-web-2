ee1f4e925814a56316a98ea97e14796b8db448c2
Deepak Mahana <mahana.deepak20@gmail.com>
2017-06-15 22:41:52 +0530

    Same Code

---
+++ models/searchkeys.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var searchSchema = new Schema({
+
+  slug: {
+		type: String,
+		unique: true
+	},
+
+  title: {
+    type: String
+  },
+
+  type: {
+    type: String
+  },
+
+  keywords: [String],
+
+
+}, {timestamps: true});
+
+searchSchema.index({keywords:'text', title:'text'});
+
+var search = mongoose.model('search', searchSchema);
+
+module.exports = search;
