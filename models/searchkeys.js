61bd38ce28b01a83eac7b8064283e949459b1156
dexuiz <dex345z@gmail.com>
2017-07-13 21:15:07 +0530

    merge v4

---
+++ models/searchkeys.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var searchSchema = new Schema({
+
+  slug: {
+        type: String,
+        unique: true
+    },
+
+  title: {
+    type: String
+  },
+
+  type: {
+    type: String
+  },
+
+  img: {
+    type: String
+  },
+
+  img_thumb: {
+    type: String
+  },
+
+
+  keywords: [String]
+
+
+}, {timestamps: true});
+
+searchSchema.index({keywords:'text', title:'text'});
+
+var search = mongoose.model('search', searchSchema);
+
+module.exports = search;
